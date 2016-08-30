809af3b5e8c3a8ce7d468b1dc5dd1ef34e2e1043
SausageParty <SausageParty@Elaines-MacBook-Air.local>
2016-08-30 18:33:21 -0400

    emails

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import re
+import csv
+
+with open('/users/sausageparty/dsp/python/faculty.csv') as f:
+    data = f.read()
+
+search_email = re.findall('[\w|\.]+@[\w|\.]+', data)
+print(search_email)
+
+with open('/users/sausageparty/dsp/python/emails.csv', 'wb') as fp:
+    wr = csv.writer(fp)
+    for se in search_email:
+        wr.writerow([se])
